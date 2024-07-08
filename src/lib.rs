6c34408cd7ebb3aaf16027fbfda2fb3f4bab7a6c
rcxid <gc6174@gmail.com>
2024-07-08 23:23:11 +0800

    feat: init

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use mlua::prelude::*;
+
+fn hello(_: &Lua, name: String) -> LuaResult<()> {
+    println!("hello, {}!", name);
+    Ok(())
+}
+
+#[mlua::lua_module]
+fn nvim_lib(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    exports.set("hello", lua.create_function(hello)?)?;
+    Ok(exports)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
